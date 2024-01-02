d16e3a06201e6a59a06905de515de33f5c4c521e
Niels Praet <niels.praet001@gmail.com>
2024-01-02 23:31:27 +0100

    build: add missing file to build

--- premake5.lua
+++ premake5.lua
@@ -7,8 +7,8 @@ objdir("bin-int/" .. outputdir .. "/%{prj.name}")
 
 files {
     "imconfig.h", "imgui.h", "imgui.cpp", "imgui_draw.cpp", "imgui_internal.h",
-    "imgui_widgets.cpp", "imstb_rectpack.h", "imstb_textedit.h",
-    "imstb_truetype.h", "imgui_demo.cpp"
+    "imgui_widgets.cpp", "imgui_tables.cpp", "imstb_rectpack.h",
+    "imstb_textedit.h", "imstb_truetype.h", "imgui_demo.cpp"
 }
 
 filter "system:windows"
